4be4159e81288cb1b3a75e13d714aef3297930bb
kaurhar65 <hkaur22@jaumebalmes.net>
2023-07-11 12:40:55 +0200

    JFGDSIUGFÑ

---
+++ MainProject/ClientApp/src/app/reservation/reservation.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation',
+  templateUrl: './reservation.component.html',
+  styleUrls: ['./reservation.component.css']
+})
