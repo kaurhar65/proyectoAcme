baad2c8ebf2f74cc1150297a5c3c0399b35683bc
SmoothPandaa <smoothpandaa@gmail.com>
2023-07-20 13:36:26 +0200

    Hecho front allroms y styles de otras cosas

--- MainProject/ClientApp/src/app/rooms/rooms.component.ts
+++ MainProject/ClientApp/src/app/rooms/rooms.component.ts
@@ -8,13 +8,30 @@ import { RequestService } from '../services/request.service';
   styleUrls: ['./rooms.component.css']
 })
 export class RoomsComponent implements OnInit {
-  rooms:any;
+  allRooms: Room[] = [
+    {id: 1, name: "Sala 1", img:"https://i.pinimg.com/736x/61/30/e3/6130e3adf957d8c9f43d48fa194c5274.jpg"},
+    {id: 2, name: "Sala 2", img:"https://i.pinimg.com/736x/8e/7d/bd/8e7dbdbf6bae48b3b63c637ffdce8bee.jpg"},
+    {id: 3, name: "Sala 3", img:"https://i.pinimg.com/736x/c5/7e/9a/c57e9ac7b836d2f822b0d72190b25080--show-rooms-meeting-rooms.jpg"},
+    {id: 4, name: "Sala 4", img:"https://i.pinimg.com/736x/c5/7e/9a/c57e9ac7b836d2f822b0d72190b25080--show-rooms-meeting-rooms.jpg"},
+  ]
+
+  
 
   constructor (private crudService: RequestService) {}
 
   ngOnInit(): void {
-    this.crudService.get("http://localhost:5075/api/Room/GetAllRooms").subscribe(data => {
-      console.table(data);
-    })
+    
+  }
+}
+
+class Room {
+  id:number;
+  name:string;
+  img:string;
+
+  constructor (id:number, name:string, img:string) {
+    this.id = id;
+    this.name = name;
+    this.img = img;
   }
 }
