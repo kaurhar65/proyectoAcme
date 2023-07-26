fa50f6e7ed14f4706c87f8c9cd0c62892991f689
Cèlia Casanovas <celiacasanovaspriego@gmail.com>
2023-07-26 12:30:24 +0200

    added claims auth for reservations, added reservations extended dto

---
+++ RoomsApiCrudBackEnd/Models/ReservationExtendedDTOModel.cs
@@ -0,0 +1,14 @@
+﻿namespace RoomsApiCrudIdentity;
+
+public class ReservationExtendedDTOModel
+{
+    public int Id { get; set; } = default!;
+    public string? Date { get; set; } = null;
+    public string? StartTime { get; set; } = null;
+    public string? EndTime { get; set; } = null;
+    public string? RoomName { get; set; } = null;
+    public string? OfficeName { get; set; } = null;
+    public string? CityName { get; set; } = null;
+    public string? CountryName { get; set; } = null; 
+    public string? UserId { get; set; } = null;
+}
