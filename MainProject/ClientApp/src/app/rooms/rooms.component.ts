bde3f1dcc5ffe27afd5141a7fdb9c0862f255823
SmoothPandaa <smoothpandaa@gmail.com>
2023-07-27 10:51:31 +0200

    Revision all rooms

--- MainProject/ClientApp/src/app/rooms/rooms.component.ts
+++ MainProject/ClientApp/src/app/rooms/rooms.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { RequestService } from '../services/request.service';
+import { apiControllers, apiUrls, environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-rooms',
@@ -9,30 +10,33 @@ import { RequestService } from '../services/request.service';
 })
 
 export class RoomsComponent implements OnInit {
-  allRooms: Room[] = [
-    {id: 1, name: "Sala 1", img:"https://i.pinimg.com/736x/61/30/e3/6130e3adf957d8c9f43d48fa194c5274.jpg"},
-    {id: 2, name: "Sala 2", img:"https://i.pinimg.com/736x/8e/7d/bd/8e7dbdbf6bae48b3b63c637ffdce8bee.jpg"},
-    {id: 3, name: "Sala 3", img:"https://i.pinimg.com/736x/c5/7e/9a/c57e9ac7b836d2f822b0d72190b25080--show-rooms-meeting-rooms.jpg"},
-    {id: 4, name: "Sala 4", img:"https://i.pinimg.com/736x/c5/7e/9a/c57e9ac7b836d2f822b0d72190b25080--show-rooms-meeting-rooms.jpg"},
-  ]
+
+  allRooms: Room[]= [];
+  constructor (private requestService: RequestService) {
+
+  }
 
   
-
-  constructor (private crudService: RequestService) {}
-
   ngOnInit(): void {
-    
+    this.requestService.get(`${environment.apiUrl}${apiControllers.room}${apiUrls.room.getAllRooms}`)
+    .subscribe({next(response: any) {
+      let allRooms: Room[] = [];
+      response.forEach((room: any) => {
+        let newRoom = new Room(room.id,room.name, room.officeId);
+        allRooms.push(newRoom);
+      });
+    }, error(err: Error){alert(`${err.name}: ${err.message}`)}})
   }
 }
 
 class Room {
   id:number;
   name:string;
-  img:string;
+  officeId:number;
 
-  constructor (id:number, name:string, img:string) {
+  constructor (id:number, name:string, officeId:number) {
     this.id = id;
     this.name = name;
-    this.img = img;
+    this.officeId = officeId;
   }
 }
