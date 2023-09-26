31753ebb5c3f52f1ff938d35aa700d1eb6e87a64
jolichh <113618956+jolichh@users.noreply.github.com>
2023-09-26 12:02:38 +0200

    second try

--- MainProject/acme-rooms/src/app/view-all/view-all.component.ts
+++ MainProject/acme-rooms/src/app/view-all/view-all.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { RequestService } from 'src/app/services/request.service';
+import {
+  environment,
+  apiControllers,
+  apiUrls,
+} from '../../environments/environment';
+import { Room } from '../models/room';
 
 @Component({
   selector: 'app-view-all',
