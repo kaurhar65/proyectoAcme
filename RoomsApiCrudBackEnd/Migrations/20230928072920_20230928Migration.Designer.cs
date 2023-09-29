08f2c4bc9fbf0a201fb092f981a18798a9c52a86
kaurhar65 <hkaur22@jaumebalmes.net>
2023-09-29 09:21:40 +0200

    Cambios hello-user, se puede cambiar el nombre del usuario

--- RoomsApiCrudBackEnd/Migrations/20230928072920_20230928Migration.Designer.cs
+++ RoomsApiCrudBackEnd/Migrations/20230928072920_20230928Migration.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RoomsApiCrudIdentity.Data;
 
@@ -10,9 +11,11 @@ using RoomsApiCrudIdentity.Data;
 namespace RoomsApiCrudIdentity.Migrations
 {
     [DbContext(typeof(RoomsApiCrudDbContext))]
-    partial class RoomsApiCrudDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230928072920_20230928Migration")]
+    partial class _20230928Migration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
