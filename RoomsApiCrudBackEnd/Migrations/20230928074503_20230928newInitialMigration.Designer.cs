ad0adf3b1723c8167e1a4e9be336f91982fab9f6
jolichh <113618956+jolichh@users.noreply.github.com>
2023-09-29 09:19:20 +0200

    View all rooms ya muestra datos

--- RoomsApiCrudBackEnd/Migrations/20230928074503_20230928newInitialMigration.Designer.cs
+++ RoomsApiCrudBackEnd/Migrations/20230928074503_20230928newInitialMigration.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RoomsApiCrudIdentity.Data;
 
@@ -10,9 +11,11 @@ using RoomsApiCrudIdentity.Data;
 namespace RoomsApiCrudIdentity.Migrations
 {
     [DbContext(typeof(RoomsApiCrudDbContext))]
-    partial class RoomsApiCrudDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230928074503_20230928newInitialMigration")]
+    partial class _20230928newInitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
